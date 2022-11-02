170f5438b75c8f16acb796fff3bd133061101550
Dimitar Rupov <dimitar.rupov@gmail.com>
2022-11-02 12:08:34 +0200

    Update title in frontpage

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Players data frm Google Sheet</title>
+        <title>Next.js with data from a Google Sheet</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1>
-          Players data coming from a Google Sheet.
+          Next.js with data from a Google Sheet
         </h1>
         <Link href='/players'>Click here</Link> to view the players.
       </main>
